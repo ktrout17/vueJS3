8a96437aa998944834fea03fe62c5beffe8fc99e
Kristin Trout <kristintrout1@gmail.com>
2021-06-21 15:04:10 +0200

    Ninja Reaction Timer Project Complete

--- Section 4/reaction-timer/src/components/Block.vue
+++ Section 4/reaction-timer/src/components/Block.vue
@@ -1,10 +1,42 @@
 <template>
-  <div class="block">Click Me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
 </template>
 
 <script>
 export default {
-    props: [ 'delay' ]
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    // once component is mounted, timer is started until user clicks the box & once it's clicked the timer is cleared
+    mounted() {
+        console.log('component mounted.');
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+        }, this.delay);
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10;
+            }, 10);
+        },
+        stopTimer() {
+            clearInterval(this.timer);
+            this.$emit('end', this.reactionTime);
+        }
+    },
+    updated() {
+        console.log('component updated.');
+    },
+    unmounted() {
+        console.log('unmounted.');
+    },
 }
 </script>
 
