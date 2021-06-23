85c988524a7b37352aa656e6aa606c982b9e2387
Kristin Trout <kristintrout1@gmail.com>
2021-06-23 10:11:18 +0200

    Section 6 - Router Basics

--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 
 Udemy:
 https://www.udemy.com/course/build-web-apps-with-vuejs-firebase/learn/lecture/23392303#overview
-
-YouTube
-https://www.youtube.com/watch?v=ccsz9FRy-nk&list=PLC3y8-rFHvwgeQIfSDtEGVvvSEPDkL_1f
-
-
---
+++ Section 6/ninja-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sapiente saepe culpa consequatur repellat facere odit, ad similique dignissimos hic itaque assumenda, fugit cum possimus, quam necessitatibus sed? Molestias autem neque perspiciatis velit laudantium. Voluptate eveniet cumque nulla. Ad quos unde rem labore, cum consequuntur quaerat iusto, tenetur porro voluptas aliquid! Debitis amet nisi tenetur corrupti aliquid. Eum fugiat accusantium dolorum vero quam in explicabo. Iste minus nobis ab sunt voluptate? Eligendi, laboriosam sunt. Non quod eveniet ipsa, vitae laudantium repudiandae tempore recusandae nobis illum! Impedit, quidem. Laudantium, voluptatem quisquam animi provident labore optio nobis odit corrupti voluptatibus, corporis magnam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sapiente saepe culpa consequatur repellat facere odit, ad similique dignissimos hic itaque assumenda, fugit cum possimus, quam necessitatibus sed? Molestias autem neque perspiciatis velit laudantium. Voluptate eveniet cumque nulla. Ad quos unde rem labore, cum consequuntur quaerat iusto, tenetur porro voluptas aliquid! Debitis amet nisi tenetur corrupti aliquid. Eum fugiat accusantium dolorum vero quam in explicabo. Iste minus nobis ab sunt voluptate? Eligendi, laboriosam sunt. Non quod eveniet ipsa, vitae laudantium repudiandae tempore recusandae nobis illum! Impedit, quidem. Laudantium, voluptatem quisquam animi provident labore optio nobis odit corrupti voluptatibus, corporis magnam!</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
