665f7702b35f47166b9c086607d02e5807957c53
Kristin Trout <kristintrout1@gmail.com>
2021-06-23 11:07:27 +0200

    Section 6(7) - json server

--- Section 6/ninja-jobs/src/views/jobs/JobDetails.vue
+++ Section 6/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,19 +1,34 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The Job ID is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The Job ID is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading Job Details...</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // },
-}
+  props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    // fetch data and return in json format - asynchronous (running in parallel with other functions) & returns promise
+    fetch("http://localhost:3000/jobs/" + this.id)
+      // once data is fetched -> pass it into JS
+      .then((res) => res.json())
+      // receive data & populate array with the data
+      .then((data) => (this.job = data))
+      // if there are any errors -> log out message
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
-
 </style>
--- Section 6/ninja-jobs/src/views/jobs/Jobs.vue
+++ Section 6/ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,42 +1,53 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }"> 
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
         <h2>{{ job.title }}</h2>
       </router-link>
+    </div>
+  </div>
+  <div v-else>
+      <p>Loading Jobs...</p>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            jobs: [
-                { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-                { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-                { title: 'Ninja Vue Developer', id: 2, details: 'lorem' },
-            ]
-        }
-    },
-}
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    // fetch data and return in json format - asynchronous (running in parallel with other functions) & returns promise
+    fetch("http://localhost:3000/jobs")
+      // once data is fetched -> pass it into JS
+      .then((res) => res.json())
+      // receive data & populate array with the data
+      .then((data) => (this.jobs = data))
+      // if there are any errors -> log out message
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
 .job h2 {
-    background: #f4f4f4;
-    padding: 20px;
-    border-radius: 10px;
-    margin: 10px auto;
-    max-width: 600px;
-    cursor: pointer;
-    color: #444;
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
 }
 
 .job h2:hover {
-    background: #ddd;
+  background: #ddd;
 }
 
 .job a {
-    text-decoration: none;
+  text-decoration: none;
 }
 </style>
